cc2f6d62ef36c1ab7175bae689396525ea72ea97
Emil Højlund Larsen <hojlund123@users.noreply.github.com>
2023-04-02 02:35:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,16 +15,35 @@ FEED_URLS = [
     'https://ing.dk/rss'
 ]
 
+DOMAIN_NAMES = {
+    'www.dr.dk': 'DR',
+    'feeds.tv2.dk': 'TV2',
+    'www.bt.dk': 'BT',
+    'ekstrabladet.dk': 'Ekstra Bladet',
+    'borsen.dk': 'Børsen',
+    'rss.dst.dk': 'DST',
+    'ing.dk': 'Ingeniøren'
+}
+
 @app.route('/')
 def index():
     # Parse the RSS feeds and get the latest news articles from each source
     articles = []
     for feed_url in FEED_URLS:
         feed = feedparser.parse(feed_url)
-        articles.extend(feed['entries'])
+        for entry in feed['entries']:
+            # Extract the domain name from the article's link URL
+            domain = urlparse(entry.link).netloc
+            # Map the domain name to a custom name (if it exists)
+            name = DOMAIN_NAMES.get(domain, domain)
+            # Add the custom name to the article dictionary
+            entry['name'] = name
+            # Add the article to the list
+            articles.append(entry)
     # Sort the articles by date (newest first)
     articles = sorted(articles, key=lambda article: article.published_parsed, reverse=True)
     return render_template('index.html', articles=articles)
+
 
 @app.route('/article/<int:article_id>')
 def article(article_id):
